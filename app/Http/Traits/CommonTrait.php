d26d4038c9b2d070b5637b11496f186ff286307a
Vipul Kumar <vipulk@chetu.com>
2016-10-12 23:31:44 +0530

    Remove dead code from files

--- app/Http/Traits/CommonTrait.php
+++ app/Http/Traits/CommonTrait.php
@@ -1,20 +1,29 @@
 <?php
+
 namespace App;
+
 namespace App\Http\Traits;
 use App\Order;
 use App\OrderDetail;
 use App\ApiData;
 use App\ApiSetting;
 
+/**
+ * This trait is used for the code redundancy & consistency for controllers
+ *
+ * @category App\Http\Traits;
+ *
+ * @return null
+ */
 trait CommonTrait {
-  
+
     /**
      * Save the order from the makePayment function
      *
      *  */
-    public function saveOrder($cart, $payment_id, $order_unique_id) {   
-        
-        foreach($cart['category_list'] as $key => $category){
+    public function saveOrder($cart, $payment_id, $order_unique_id) {
+
+        foreach ($cart['category_list'] as $key => $category) {
             $order = new Order;
             $order->payment_id = $payment_id;
             $order->category_id = $category['category_id'];
@@ -25,7 +34,7 @@ trait CommonTrait {
             $order->discount_price = $cart['package_discount'][$key];
             $order->save();
             /* ---------------START Save the data into the orderdetail table regarding the package order ------- */
-            foreach($cart['category_detail_list'][$key] as $product){
+            foreach ($cart['category_detail_list'][$key] as $product) {
                 $orderDetail = new OrderDetail;
                 $orderDetail->order_id = $order->id;
                 $orderDetail->product_sku = $product['sku'];
@@ -33,14 +42,14 @@ trait CommonTrait {
                 $orderDetail->quantity = $product['count'];
                 $orderDetail->unit_price = $product['original_price'];
                 $orderDetail->discount_price = $product['discount_price'];
-                $orderDetail->save();   
+                $orderDetail->save();
                 unset($orderDetail);
             }
             unset($order);
             /* -------------- END ------------- */
-        }        
+        }
     }
-    
+
     /**
      * Save the API Data in api_details table under managementusa-hc-api database.
      *
@@ -50,18 +59,15 @@ trait CommonTrait {
         $json_data = $this->getResponse($data);
         $datas = json_decode($json_data, true);
         $error_row = [];
-        if(isset($datas['Data']) && !empty($datas['Data']))
-        {
+        if (isset($datas['Data']) && !empty($datas['Data'])) {
             $arr = $datas['Data'];
             $maxTimeStamp = \DB::connection('mysql2')->table('api_data')->max('timestamp');
-            foreach($arr as $data)
-            {
-                $timestamp = preg_replace("/[^0-9]/","",$data['DateTime']);
+            foreach ($arr as $data) {
+                $timestamp = preg_replace("/[^0-9]/", "", $data['DateTime']);
 
-                if($timestamp > $maxTimeStamp)
-                {
+                if ($timestamp > $maxTimeStamp) {
                     $apiData = new ApiData;
-                    $datatime = date('Y-m-d h:i:s', ($timestamp/1000));
+                    $datatime = date('Y-m-d h:i:s', ($timestamp / 1000));
 
                     $apiData->timestamp = $timestamp;
                     $apiData->date_time = $datatime;
@@ -100,17 +106,14 @@ trait CommonTrait {
                     }
                 }
             }
-            if(count($error_row))
-            {
+            if (count($error_row)) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
     }
-    
+
     /**
      * Save the API Setting in api_setting table under managementusa-hc-api database.
      *
@@ -123,14 +126,13 @@ trait CommonTrait {
                 'user_name' => 'required',
                 'password' => 'required'
             ]);
-        
+
             $apiSetting = new ApiSetting;
             $apiSetting->api_url = $request->api_url;
             $apiSetting->user_name = $request->user_name;
-            $apiSetting->password =  $request->password;
+            $apiSetting->password = $request->password;
             $saved = $apiSetting->save();
-        }
-        else {
+        } else {
             $this->validate($request, [
                 'api_url' => 'required',
                 'user_name' => 'required',
@@ -139,16 +141,16 @@ trait CommonTrait {
             $input = $request->all();
             $saved = $api_setting->fill($input)->save();
         }
-        
+
         return $saved;
     }
-    
+
     /**
      * Function is used to get the response from hosted number API.
      *
      * @return json data
      */
-    protected function getResponse($data){
+    protected function getResponse($data) {
         $username = $data->user_name;
         $password = $data->password;
 
@@ -157,17 +159,16 @@ trait CommonTrait {
         $url = $data->api_url;
 
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_FOLLOWLOCATION, false);
-        curl_setopt($ch,CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 30);
-        if($method == 'POST')
-        {
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        if ($method == 'POST') {
             $fieldsData = http_build_query($fields);
-            curl_setopt($ch,CURLOPT_POSTFIELDS, $fieldsData);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsData);
         }
 
         curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -176,54 +177,51 @@ trait CommonTrait {
 
         preg_match('/WWW-Authenticate: Digest (.*)/', $first_response, $matches);
 
-        if(!empty($matches))
-        {
+        if (!empty($matches)) {
             $auth_header = $matches[1];
             $auth_header_array = explode(',', $auth_header);
             $parsed = array();
 
-            foreach ($auth_header_array as $pair)
-            {
+            foreach ($auth_header_array as $pair) {
                 $vals = explode('=', $pair);
                 $parsed[trim($vals[0])] = trim($vals[1], '" ');
             }
 
-            $response_realm     = (isset($parsed['realm'])) ? $parsed['realm'] : "";
-            $response_nonce     = (isset($parsed['nonce'])) ? $parsed['nonce'] : "";
-            $response_opaque    = (isset($parsed['opaque'])) ? $parsed['opaque'] : "";
+            $response_realm = (isset($parsed['realm'])) ? $parsed['realm'] : "";
+            $response_nonce = (isset($parsed['nonce'])) ? $parsed['nonce'] : "";
+            $response_opaque = (isset($parsed['opaque'])) ? $parsed['opaque'] : "";
 
-            $authenticate1 = md5($username.":".$response_realm.":".$password);
-            $authenticate2 = md5($method.":".$url);
+            $authenticate1 = md5($username . ":" . $response_realm . ":" . $password);
+            $authenticate2 = md5($method . ":" . $url);
 
-            $authenticate_response = md5($authenticate1.":".$response_nonce.":".$authenticate2);
+            $authenticate_response = md5($authenticate1 . ":" . $response_nonce . ":" . $authenticate2);
 
-            $request = sprintf('Authorization: Digest username="%s", realm="%s", nonce="%s", opaque="%s", uri="%s", response="%s"',
-            $username, $response_realm, $response_nonce, $response_opaque, $url, $authenticate_response);
+            $request = sprintf('Authorization: Digest username="%s", realm="%s", nonce="%s", opaque="%s", uri="%s", response="%s"', $username, $response_realm, $response_nonce, $response_opaque, $url, $authenticate_response);
 
             $request_header = array($request);
 
             $ch = curl_init();
-            curl_setopt($ch,CURLOPT_URL, $url);
-            curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch,CURLOPT_FOLLOWLOCATION, false);
-            curl_setopt($ch,CURLOPT_TIMEOUT, 30);
-            curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 
-            if($method == 'POST')
-            {
+            if ($method == 'POST') {
                 $fieldsData = http_build_query($fields);
-                curl_setopt($ch,CURLOPT_POSTFIELDS, $fieldsData);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsData);
             }
             curl_setopt($ch, CURLOPT_HTTPHEADER, $request_header);
 
-            $result['response']         = curl_exec($ch);
-            $result['info']             = curl_getinfo ($ch);
-            $result['info']['errno']    = curl_errno($ch);
-            $result['info']['errmsg']   = curl_error($ch); 
-            
+            $result['response'] = curl_exec($ch);
+            $result['info'] = curl_getinfo($ch);
+            $result['info']['errno'] = curl_errno($ch);
+            $result['info']['errmsg'] = curl_error($ch);
+
             return $result['response'];
-        } 
+        }
     }
+
 }
